118d8c5497a330481660cbd5a095b8974051fa05
v_ljianlliu <v_ljianlliu@tencent.com>
2019-08-20 23:13:14 +0800

    init

--- src/code/string/isValidString.js
+++ src/code/string/isValidString.js
@@ -8,6 +8,25 @@
   左括号必须以正确的顺序闭合。
   注意空字符串可被认为是有效字符串。
  */
-var isValid = function(s) {
-    
+const isValid = function(s) {
+  // const map = {
+  //   '(': ')',
+  //   '{': '}',
+  //   '[': ']',
+  // };
+  while(s.length) {
+    const temp = s;
+    s.replace('()', '');
+    console.log('s', s);
+    s.replace('[]', '');
+    console.log('s', s);
+    s.replace('{}', '');
+    console.log('s', s);
+    if (s == temp) {
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(isValid("{[]}"));
